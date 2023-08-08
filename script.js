75f28c468e830cee636f1c7ae0853e945529f286
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-08-08 14:58:02 -0300

    adicionando mensagens de error

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+const requiredInput = document.querySelectorAll('.required')
+const requiredSpan  = document.querySelectorAll('.span-required')
+const colorLabel    = document.querySelectorAll('.colorLabel')
+
+function setError(index) {
+  requiredInput[index].style.border = 'solid 2px #e63636'
+  requiredSpan[index].style.display = 'block'
+  colorLabel[index].style.color     = '#e63636'
+}
+
+function removeError(index) {
+    requiredInput[index].style.border = ''
+    requiredSpan[index].style.display = 'none'
+    colorLabel[index].style.color     = 'rgb(117, 117, 117)'
+}
+
+function icardnumberValidate() {
+  if (requiredInput[0].value.length > 16 || requiredInput[0].value.length == '') {
+    setError(0)
+  } else {
+    removeError(0)
+  }
+}
+
+function imesValidate() {
+  if (requiredInput[1].value.length > 2 || requiredInput[1].value.length == '') {
+    setError(1)
+  } else {
+    removeError(1)
+  }
+}
+
+function ianoValidate() {
+  if (requiredInput[2].value.length > 2 || requiredInput[2].value.length == '') {
+    setError(2)
+  } else {
+    removeError(2)
+  }
+}
+
+function icvcValidate() {
+  if (requiredInput[3].value.length > 3 || requiredInput[3].value.length == '') {
+    setError(3)
+  } else {
+    removeError(3)
+  }
+}
+
 function updateCardInfo() {
     // Atualizar o nome do titular do cartão
     var cardNameInput = document.getElementById("icardname");
